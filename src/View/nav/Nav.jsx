13f84bc87dee7a998782ab92062e11269e234cc5
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-01 14:42:01 -0300

    fixed styles nav

--- src/View/nav/Nav.jsx
+++ src/View/nav/Nav.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import '../../Styles/NavStyles.css'
 const Nav = () => {
   return (
-    <nav>
-        <h1>Test</h1>
-        <ul>
+    <nav className='containerNav'>
+        <h1 className='logoNav'>TEST</h1>
+        <ul className='routesNav'>
             <li><Link to='/home'>Home</Link></li>
             <li><Link to='/code'>Code</Link></li>
             <li><Link to='/about'>About</Link></li>
