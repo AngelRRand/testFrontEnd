f5de5decdead0eb6cfa030a844f67029ff6b3796
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-10-03 01:52:49 -0300

    finishPagination

--- src/Pages/home/Component/ArticlesList.jsx
+++ src/Pages/home/Component/ArticlesList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import ReactPaginate from 'react-paginate';
 import ArticleContext from '../../../Context/Article/ArticleContext.jsx';
-import Spinner from './Spinner.jsx';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import '../../../Styles/home/PaginationStyles.css'
 import json from './articles.json'
 import ArticlesListMap from './ArticlesListMap.jsx';
@@ -29,7 +29,22 @@ function ArticlesList() {
         const newOffset = (event.selected * itemsPerPage) % data.length;
         setItemOffset(newOffset);
     };
-
+    let arrowleft = () =>{
+        return (
+            <div className='arrow'>
+                <AiOutlineArrowLeft/>
+                <p>Previous</p>
+            </div>
+        )
+    }
+    let arrowrigth = () =>{
+        return (
+            <div className='arrow'>
+                <p>Next</p>
+                <AiOutlineArrowRight/>
+            </div>
+        )
+    }
     return (
         <>
             <ArticlesListMap
@@ -38,11 +53,11 @@ function ArticlesList() {
             <div className='centerPagination'>
                 <ReactPaginate
                     breakLabel="..."
-                    nextLabel="next ->"
+                    nextLabel={arrowrigth()}
+                    previousLabel={arrowleft() }
                     onPageChange={handlePageClick}
                     pageRangeDisplayed={3}
                     pageCount={pageCount}
-                    previousLabel="<- previous"
                     renderOnZeroPageCount={null}
                     containerClassName='pagination'
                     pageClassName='containerPageNum'
